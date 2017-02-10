0f32fed355e265d5f7a0aa03a8d0bd8e26bec013
Abhishek Rathore <rathore.abhishek87@gmail.com>
2017-02-10 15:19:56 +0530

    local mongo

---
+++ server/models/user.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(User) {
+User.on('resetPasswordRequest', function (info) {
+  console.log(info.email); // the email of the requested user
+  console.log(info.accessToken.id); // the temp access token to allow password reset
+
+  // requires AccessToken.belongsTo(User)
+  info.accessToken.user(function (err, user) {
+    console.log(user); // the actual user
+  });
+});
+};
