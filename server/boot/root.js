997a8089fcf6e0fe02623097f1d85baa0496cdeb
Abhishek Rathore <rathore.abhishek87@gmail.com>
2017-02-10 16:20:17 +0530

    reset password api

--- server/boot/root.js
+++ server/boot/root.js
@@ -2,7 +2,42 @@
 
 module.exports = function(server) {
   // Install a `/` route that returns server status
+
+  var User = server.models.User;
+
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+  
+  router.post('/reset-password', function(req, res, next) {
+  	// console.log(req);
+   //  if (!req.accessToken) return res.sendStatus(401);
+
+   //  //verify passwords match
+   //  if (!req.body.password ||
+   //      !req.body.confirmation ||
+   //      req.body.password !== req.body.confirmation) {
+   //    return res.sendStatus(400, new Error('Passwords do not match'));
+   //  }
+
+    User.findById(req.body.userId, function(err, user) {
+      if (err) return res.sendStatus(404);
+      user.updateAttribute('password', req.body.password, function(err, user) {
+      if (err) return res.sendStatus(404);
+        console.log('> password reset processed successfully');
+        res.render('response', {
+          title: 'Password reset success',
+          content: 'Your password has been reset successfully',
+          redirectTo: '/',
+          redirectToLinkText: 'Log in'
+        });
+      });
+    });
+  });
+
+
   server.use(router);
+
+
+
+
 };
