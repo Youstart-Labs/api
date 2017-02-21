60b1e0f3b7eed29036a7130d9725fdf0d36a4c82
rohanhacker <rohan.root@gmail.com>
2017-02-21 12:51:05 +0530

    added acls

---
+++ server/boot/routes.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = function(server) {
+  // Install a `/` route that returns server status
+
+  var User = server.models.User;
+
+  var router = server.loopback.Router();
+  router.get('/', server.loopback.status());
+  
+  router.post('/reset-password', function(req, res, next) {
+    console.log(req);
+    if (!req.accessToken) return res.sendStatus(401);
+    //verify passwords match
+    if (!req.body.password ||
+        !req.body.confirmation ||
+        req.body.password !== req.body.confirmation) {
+      return res.sendStatus(400, new Error('Passwords do not match'));
+    }
+
+    User.findById(req.accessToken.userId, function(err, user) {
+      if (err) return res.sendStatus(404);
+      user.updateAttribute('password', req.body.password, function(err, user) {
+      if (err) return res.sendStatus(404);
+        console.log('> password reset processed successfully');
+        res.render('response', {
+          title: 'Password reset success',
+          content: 'Your password has been reset successfully',
+          redirectTo: '/',
+          redirectToLinkText: 'Log in'
+        });
+      });
+    });
+  });
+
+
+  server.use(router);
+
+
+
+
+};
